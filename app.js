e22b4009427f8c283665c4b23f40db984a9d44dd
LeoAraya25 <delacruaraya25@gmail.com>
2023-08-25 09:17:16 -0300

    agregando contenido

--- app.js
+++ app.js
@@ -1,77 +1,102 @@
-// Función para agregar una operación al carrito
-function agregarOperacion() {
-  const tipoContenedor = parseInt(document.getElementById('tipoContenedor').value, 10);
-  const cantidad = parseInt(document.getElementById('cantidad').value, 10);
-  const paisDestino = document.getElementById('paisDestino').value;
+document.addEventListener("DOMContentLoaded", function () {
+  const containerSizeSelect = document.getElementById("containerSize");
+  const destinationSelect = document.getElementById("destination");
+  const quantityInput = document.getElementById("cantidadContenedores");
+  const calculateButton = document.getElementById("calculateBtn");
+  const cartItemsList = document.getElementById("cartItems");
+  const totalCostElement = document.getElementById("totalCost");
+  const checkoutButton = document.getElementById("checkoutBtn");
+  const clearCartButton = document.getElementById("clearCartBtn");
 
-  const order = new Order(tipoContenedor, cantidad, paisDestino);
-  const precioTotal = order.calcularPrecio();
+  const cart = [];
 
-  const operacion = {
-    tipoContenedor,
-    cantidad,
-    paisDestino,
-    precioTotal
-  };
+  function updateTotalCost() {
+    const totalCost = cart.reduce((total, item) => total + item.cost, 0);
+    totalCostElement.textContent = totalCost.toFixed(2);
+  }
 
-  cartItems.push(operacion);
+  function updateCartItems() {
+    cartItemsList.innerHTML = "";
+    cart.forEach((item, index) => {
+      const li = document.createElement("li");
+      li.textContent = `${item.size} pies a ${item.destination} - Costo: U$S${item.cost.toFixed(2)}`;
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Eliminar";
+      removeButton.addEventListener("click", () => {
+        cart.splice(index, 1);
+        updateCartItems();
+        updateTotalCost();
+      });
+      li.appendChild(removeButton);
+      cartItemsList.appendChild(li);
+    });
+  }
 
-  Swal.fire({
-    icon: 'success',
-    title: 'Operación agregada al carrito',
-    text: `Se agregó una operación con ${cantidad} contenedor(es) de tipo ${tipoContenedor} a ${paisDestino}. Precio total: U$S${precioTotal}`,
-    confirmButtonText: 'OK'
+  calculateButton.addEventListener("click", () => {
+    const selectedSize = containerSizeSelect.value;
+    const selectedDestination = destinationSelect.value;
+    const quantity = parseInt(quantityInput.value, 10);
+
+    let cost = 0;
+    if (selectedSize === "20") {
+      cost += 1000;
+    } else if (selectedSize === "40") {
+      cost += 2000;
+    }
+
+    if (selectedDestination === "China") {
+      cost += 900;
+    } else if (selectedDestination === "Estados Unidos") {
+      cost += 800;
+    } else if (selectedDestination === "Reino Unido") {
+      cost += 900;
+    } else if (selectedDestination === "España") {
+      cost += 700;
+    } else if (selectedDestination === "Chile") {
+      cost += 400;
+    } else if (selectedDestination === "Brasil") {
+      cost += 450;
+    }
+
+    const item = {
+      size: selectedSize,
+      destination: selectedDestination,
+      cost: cost * quantity,
+    };
+
+    cart.push(item);
+    updateCartItems();
+    updateTotalCost();
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Agregado al carrito',
+      text: `Se agregaron ${quantity} contenedor(es) de ${selectedSize} pies a ${selectedDestination}.`
+    });
   });
 
-  actualizarCarritoUI();
-}
-
-// Función para vaciar el carrito
-function vaciarCarrito() {
-  cartItems.length = 0; // Vaciar el array
-  actualizarCarritoUI(); // Actualizar la interfaz de usuario
-}
-
-// Función para actualizar el carrito en el UI
-function actualizarCarritoUI() {
-  const cartItemsList = document.getElementById('cartItems');
-  cartItemsList.innerHTML = '';
-
-  let totalCost = 0;
-
-  cartItems.forEach((operacion, index) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = `Contenedor: ${operacion.tipoContenedor} - Destino: ${operacion.paisDestino} - Cantidad: ${operacion.cantidad}`;
-    cartItemsList.appendChild(listItem);
-
-    totalCost += operacion.precioTotal;
+  clearCartButton.addEventListener("click", () => {
+    cart.length = 0;
+    updateCartItems();
+    updateTotalCost();
   });
 
-  const totalCostSpan = document.getElementById('totalCost');
-  totalCostSpan.textContent = totalCost.toFixed(2);
-}
-
-// Event listener para el botón "Agregar a la operación"
-const calculateBtn = document.getElementById('calculateBtn');
-calculateBtn.addEventListener('click', agregarOperacion);
-
-// Event listener para el botón "Vaciar Carrito"
-const clearCartBtn = document.getElementById('clearCartBtn');
-clearCartBtn.addEventListener('click', () => {
-  Swal.fire({
-    icon: 'warning',
-    title: '¿Estás seguro de vaciar el carrito?',
-    showCancelButton: true,
-    confirmButtonText: 'Sí, vaciar',
-    cancelButtonText: 'Cancelar'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      vaciarCarrito();
+  checkoutButton.addEventListener("click", () => {
+    if (cart.length === 0) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Carrito vacío',
+        text: 'Agrega elementos al carrito antes de finalizar la operación.',
+      });
+    } else {
       Swal.fire({
         icon: 'success',
-        title: 'Carrito vaciado',
-        text: 'El carrito se ha vaciado exitosamente',
-        confirmButtonText: 'OK'
+        title: 'Operación finalizada',
+        text: `Se ha finalizado la operación con un costo total de U$S${totalCostElement.textContent}.`,
+      }).then(() => {
+        cart.length = 0;
+        updateCartItems();
+        updateTotalCost();
       });
     }
   });
